05d5e7cf3eb3c68f41f51c92a7ad2dfe2fa823e9
Saheb Ali <you@example.com>
2023-02-14 07:36:20 +0600

    change URLe

--- config/bd.js
+++ config/bd.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 const {DB_URL} = require("../config/envConfig.js");
-
+console.log(DB_URL);
 const connect = async ()=>{
     try {
          await mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 5000, });
